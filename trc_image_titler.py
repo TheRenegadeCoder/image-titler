18c9490ae655f7cb107d980d4b593f237cc5502b
Jeremy Griffith <jrg170@case.edu>
2018-03-23 01:11:52 -0400

    Moved all magic numbers to constants
    I'm sure there's some fancy function I can write to map everything the way I want it, but this works for now.

--- trc_image_titler.py
+++ trc_image_titler.py
@@ -3,7 +3,18 @@ from PIL import ImageFont
 from PIL import ImageDraw
 
 SOURCE = 'E:\\Documents\\Work\\The Renegade Coder\\Assets\\Featured Images\\Sources\\hello-world-in-swift.jpg'
-TITLE = "A Wild World Ahead"
+TITLE = "A Wild World Ahead Boy"
+FONT = "BERNHC.TTF"
+
+FONT_SIZE = 114
+TOP_RECTANGLE_Y = 145
+BOTTOM_RECTANGLE_Y = TOP_RECTANGLE_Y + 180
+TOP_TEXT_Y = TOP_RECTANGLE_Y + 5
+BOTTOM_TEXT_Y = BOTTOM_RECTANGLE_Y + 5
+RECTANGLE_HEIGHT = 145
+IMAGE_WIDTH = 1920
+IMAGE_HEIGHT = 1200
+X_OFFSET = 30
 
 
 def split_string_by_nearest_middle_space(input_string):
@@ -26,16 +37,17 @@ def split_string_by_nearest_middle_space(input_string):
 
 
 def draw_text(image, title):
-    cropped_img = image.crop((0, 0, 1920, 1200))
+    cropped_img = image.crop((0, 0, IMAGE_WIDTH, IMAGE_HEIGHT))
     draw = ImageDraw.Draw(cropped_img)
-    font = ImageFont.truetype("BERNHC.TTF", 114)
+    font = ImageFont.truetype(FONT, FONT_SIZE)
     top_half, bottom_half = split_string_by_nearest_middle_space(title)
     top_width, top_height = draw.textsize(top_half, font)
     bottom_width, bottom_height = draw.textsize(bottom_half, font)
-    draw.rectangle(((1920 - top_width - 60, 150), (1920, 150 + top_height + 20)), fill="red")
-    draw.rectangle(((1920 - bottom_width - 60, 325), (1920, 325 + bottom_height + 20)), fill="red")
-    draw.text((1920 - top_width - 30, 150), top_half, fill=(255, 255, 255), font=font)
-    draw.text((1920 - bottom_width - 30, 325), bottom_half, fill=(255, 255, 255), font=font)
+    print(top_height, bottom_height)
+    draw.rectangle(((IMAGE_WIDTH - top_width - X_OFFSET * 2, TOP_RECTANGLE_Y), (IMAGE_WIDTH, TOP_RECTANGLE_Y + RECTANGLE_HEIGHT)), fill="red")
+    draw.rectangle(((IMAGE_WIDTH - bottom_width - X_OFFSET * 2, BOTTOM_RECTANGLE_Y), (IMAGE_WIDTH, BOTTOM_RECTANGLE_Y + RECTANGLE_HEIGHT)), fill="red")
+    draw.text((IMAGE_WIDTH - top_width - X_OFFSET, TOP_TEXT_Y), top_half, fill=(255, 255, 255), font=font)
+    draw.text((IMAGE_WIDTH - bottom_width - X_OFFSET, BOTTOM_TEXT_Y), bottom_half, fill=(255, 255, 255), font=font)
     cropped_img.show()
 
 
