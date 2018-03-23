017d605e76be806b1126d937b36648b05cfa0bc0
Jeremy Griffith <jrg170@case.edu>
2018-03-23 00:30:53 -0400

    Hardcoded positions for now
    Looking to derive some sort of function from this soon

--- trc_image_titler.py
+++ trc_image_titler.py
@@ -24,13 +24,15 @@ def split_string_by_nearest_middle_space(input_string):
         n += 1
     return input_string[:index], input_string[index + 1:]
 
+
+top_half, bottom_half = split_string_by_nearest_middle_space(TITLE)
 img = Image.open(SOURCE)
 cropped_img = img.crop((0, 0, 1920, 1200))
 draw = ImageDraw.Draw(cropped_img)
 font = ImageFont.truetype("BERNHC.TTF", 114)
 width, height = draw.textsize(TITLE, font)
 print("Width: {0}, Height: {1}".format(width, height))
-draw.text((1500, 200), TITLE, fill=(255, 255, 255), font=font)
+draw.text((1350, 150), top_half, fill=(255, 255, 255), font=font)
+draw.text((1550, 300), bottom_half, fill=(255, 255, 255), font=font)
 cropped_img.show()
 
-print(split_string_by_nearest_middle_space(TITLE))
