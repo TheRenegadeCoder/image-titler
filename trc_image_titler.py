a6df4ef6c8f7dca15d193d04e776f95404eff895
Jeremy Griffith <jrg170@case.edu>
2018-03-23 00:56:08 -0400

    Playing with new strings

--- trc_image_titler.py
+++ trc_image_titler.py
@@ -3,7 +3,7 @@ from PIL import ImageFont
 from PIL import ImageDraw
 
 SOURCE = 'E:\\Documents\\Work\\The Renegade Coder\\Assets\\Featured Images\\Sources\\hello-world-in-swift.jpg'
-TITLE = "Hello World in Swift"
+TITLE = "A Wild World Ahead"
 
 
 def split_string_by_nearest_middle_space(input_string):
@@ -33,9 +33,9 @@ def draw_text(image, title):
     top_width, top_height = draw.textsize(top_half, font)
     bottom_width, bottom_height = draw.textsize(bottom_half, font)
     draw.rectangle(((1920 - top_width - 60, 150), (1920, 150 + top_height + 20)), fill="red")
-    draw.rectangle(((1920 - bottom_width - 60, 300), (1920, 300 + bottom_height + 20)), fill="red")
+    draw.rectangle(((1920 - bottom_width - 60, 325), (1920, 325 + bottom_height + 20)), fill="red")
     draw.text((1920 - top_width - 30, 150), top_half, fill=(255, 255, 255), font=font)
-    draw.text((1920 - bottom_width - 30, 300), bottom_half, fill=(255, 255, 255), font=font)
+    draw.text((1920 - bottom_width - 30, 325), bottom_half, fill=(255, 255, 255), font=font)
     cropped_img.show()
 
 
