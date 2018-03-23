94aae0f8df722560b0205170beaac5b9c904079d
Jeremy Griffith <jrg170@case.edu>
2018-03-22 22:53:55 -0400

    Added an image crop

--- trc_image_titler.py
+++ trc_image_titler.py
@@ -3,7 +3,8 @@ from PIL import ImageFont
 from PIL import ImageDraw
 
 img = Image.open('E:\\Documents\\Work\\The Renegade Coder\\Assets\\Featured Images\\Sources\\hello-world-in-swift.jpg')
-draw = ImageDraw.Draw(img)
+cropped_img = img.crop((0, 0, 1920, 1200))
+draw = ImageDraw.Draw(cropped_img)
 font = ImageFont.truetype("BERNHC.TTF", 160)
 draw.text((0, 0), "Sample Text", fill=(255, 255, 255), font=font)
-img.show()
+cropped_img.show()
