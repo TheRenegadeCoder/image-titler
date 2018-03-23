641121341c47f05d13e296a057bdb8ef9bbb54b9
Jeremy Griffith <jrg170@case.edu>
2018-03-22 23:04:55 -0400

    Messing with text size

--- trc_image_titler.py
+++ trc_image_titler.py
@@ -2,9 +2,14 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
-img = Image.open('E:\\Documents\\Work\\The Renegade Coder\\Assets\\Featured Images\\Sources\\hello-world-in-swift.jpg')
+SOURCE = 'E:\\Documents\\Work\\The Renegade Coder\\Assets\\Featured Images\\Sources\\hello-world-in-swift.jpg'
+TITLE = "Hello World in Swift"
+
+img = Image.open(SOURCE)
 cropped_img = img.crop((0, 0, 1920, 1200))
 draw = ImageDraw.Draw(cropped_img)
-font = ImageFont.truetype("BERNHC.TTF", 160)
-draw.text((0, 0), "Sample Text", fill=(255, 255, 255), font=font)
+font = ImageFont.truetype("BERNHC.TTF", 114)
+width, height = draw.textsize(TITLE, font)
+print("Width: {0}, Height: {1}".format(width, height))
+draw.text((1500, 200), TITLE, fill=(255, 255, 255), font=font)
 cropped_img.show()
