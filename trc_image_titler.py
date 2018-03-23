b829a132109a998257ccde377a0bc7b6115632cb
Jeremy Griffith <jrg170@case.edu>
2018-03-22 22:48:11 -0400

    Added text to my image

--- trc_image_titler.py
+++ trc_image_titler.py
@@ -1,5 +1,9 @@
-from PIL import Image, ImageFilter
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 
 img = Image.open('E:\\Documents\\Work\\The Renegade Coder\\Assets\\Featured Images\\Sources\\hello-world-in-swift.jpg')
-
+draw = ImageDraw.Draw(img)
+font = ImageFont.truetype("BERNHC.TTF", 160)
+draw.text((0, 0), "Sample Text", fill=(255, 255, 255), font=font)
 img.show()
