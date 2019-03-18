496ad131799ed6fcbea19d213c9f8a617002685d
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-03-18 13:50:25 -0400

    Added manifest file to include font

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/TheRenegadeCoder/image-titler",
     packages=setuptools.find_packages(),
+    include_package_data = True,
     entry_points={
         "console_scripts": [
             'image_titler = image_titler.trc_image_titler:main',
