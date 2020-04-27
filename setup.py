149e2593e4f43d69fbf5a4cca642de070acfae10
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-04-26 22:12:25 -0400

    Added Python 3.8+ restriction

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="image-titler",
-    version="1.8.0",
+    version="1.8.1",
     author="The Renegade Coder",
     author_email="jeremy.grifski@therenegadecoder.com",
     description="Adds a title and logo to an image using The Renegade Coder Featured Image style",
@@ -14,13 +14,14 @@ setuptools.setup(
     url="https://github.com/TheRenegadeCoder/image-titler",
     packages=setuptools.find_packages(),
     include_package_data=True,
+    python_requires=">=3.8",
     entry_points={
         "console_scripts": [
             'image_titler = image_titler.trc_image_titler:main',
         ],
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
