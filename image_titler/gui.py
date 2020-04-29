a8306be0ff7917a9e9c24360f0186c6b78fc971b
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-04-29 16:52:19 -0400

    Added a menu update function

--- image_titler/gui.py
+++ image_titler/gui.py
@@ -27,30 +27,38 @@ class ImageTitlerMenuBar(tk.Menu):
         self.parent = parent
         self.image_path = None
         self.current_edit = None
+        self.file_menu = None
         self.init_menu()
 
     def init_menu(self):
         menu = tk.Menu(self.parent)
         self.parent.config(menu=menu)
 
-        file_menu = tk.Menu(menu, tearoff=0)
-        file_menu.add_command(label="New Image", command=self.new_image)
-        file_menu.add_command(label="Save As", command=self.save_as)
+        self.file_menu = tk.Menu(menu, tearoff=0, postcommand=self.save_as_enabled)
+        self.file_menu.add_command(label="New Image", command=self.new_image)
+        self.file_menu.add_command(label="Save As", command=self.save_as)
 
-        menu.add_cascade(label="File", menu=file_menu)
+        menu.add_cascade(label="File", menu=self.file_menu)
 
     def new_image(self):
         self.image_path = tk.filedialog.askopenfilename()
-        title = convert_file_name_to_title(self.image_path)
-        self.current_edit = process_image(self.image_path, title)
-        image = ImageTk.PhotoImage(self.current_edit)
-        self.parent.preview.config(image=image)
-        self.parent.preview.image = image
-        self.parent.preview.pack(side="bottom", fill="both", expand="yes")
+        if self.image_path:
+            title = convert_file_name_to_title(self.image_path)
+            self.current_edit = process_image(self.image_path, title)
+            image = ImageTk.PhotoImage(self.current_edit)
+            self.parent.preview.config(image=image)
+            self.parent.preview.image = image
+            self.parent.preview.pack(side="bottom", fill="both", expand="yes")
 
     def save_as(self):
         output_path = tk.filedialog.askdirectory()
         save_copy(self.image_path, self.current_edit, output_path=output_path)
+
+    def save_as_enabled(self):
+        if self.current_edit:
+            self.file_menu.entryconfig(1, state=tk.NORMAL)
+        else:
+            self.file_menu.entryconfig(1, state=tk.DISABLED)
 
 
 def main():
