92c657e285229aed99a30e71b08278d306e2db71
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-04-29 23:57:54 -0400

    Added logo addition to the GUI

--- image_titler/gui.py
+++ image_titler/gui.py
@@ -38,15 +38,18 @@ class ImageTitlerGUI(tk.Frame):
         if self.menu.image_path:
             title = None
             tier = ""
+            logo_path = None
             if self.option_pane.title_state.get() == 1:
                 title = self.option_pane.title_value.get()
             if self.option_pane.tier_state.get() == 1:
                 tier = self.option_pane.tier_value.get()
-            self._render_preview(title=title, tier=tier)
+            if self.option_pane.logo_state.get() == 1:
+                logo_path = self.menu.logo_path
+            self._render_preview(title=title, tier=tier, logo_path=logo_path)
 
-    def _render_preview(self, title=None, tier=""):
+    def _render_preview(self, title=None, tier="", logo_path=None):
         title = convert_file_name_to_title(self.menu.image_path, title=title)
-        self.menu.current_edit = process_image(self.menu.image_path, title, tier=tier)
+        self.menu.current_edit = process_image(self.menu.image_path, title, tier=tier, logo_path=logo_path)
         maxsize = (1028, 1028)
         small_image = self.menu.current_edit.copy()
         small_image.thumbnail(maxsize, Image.ANTIALIAS)
@@ -71,32 +74,37 @@ class ImageTitlerOptionPane(tk.Frame):
         self.title_value: tk.StringVar = tk.StringVar()
         self.tier_state: tk.IntVar = tk.IntVar()
         self.tier_value: tk.StringVar = tk.StringVar()
+        self.logo_state: tk.IntVar = tk.IntVar()
         self.init_option_pane()
 
     def init_option_pane(self):
         # Title UI
         title_frame = tk.Frame(self)
-        title_frame.pack(side=tk.TOP, anchor=tk.NW, padx=10, pady=5, expand=tk.YES, fill=tk.X)
-
         title_label = tk.Checkbutton(title_frame, text="Title:", variable=self.title_state,
                                      command=self.parent.update_view)
-        title_label.pack(side=tk.LEFT, anchor=tk.N)
-
         self.title_value.trace("w", self.parent.update_view)
         title_entry = tk.Entry(title_frame, textvariable=self.title_value)
-        title_entry.pack(side=tk.LEFT, anchor=tk.N)
+        self.layout_option_row(title_frame, title_label, title_entry)
 
         # Tier UI
         tier_frame = tk.Frame(self)
-        tier_frame.pack(side=tk.TOP, anchor=tk.NW, padx=10, pady=5, expand=tk.YES, fill=tk.X)
-
         tier_label = tk.Checkbutton(tier_frame, text="Tier:", variable=self.tier_state,
                                      command=self.parent.update_view)
-        tier_label.pack(side=tk.LEFT, anchor=tk.N)
-
         self.tier_value.set(list(TIER_MAP.keys())[0])
         tier_option_menu = tk.OptionMenu(tier_frame, self.tier_value, *TIER_MAP.keys(), command=self.parent.update_view)
-        tier_option_menu.pack(side=tk.LEFT, anchor=tk.N, expand=tk.YES, fill=tk.X)
+        self.layout_option_row(tier_frame, tier_label, tier_option_menu)
+
+        # Logo UI
+        logo_frame = tk.Frame(self)
+        logo_label = tk.Checkbutton(logo_frame, text="Logo:", variable=self.logo_state, command=self.parent.update_view)
+        logo_image = tk.Label(logo_frame, text="Select a logo using 'File' > 'New Logo'")
+        self.layout_option_row(logo_frame, logo_label, logo_image)
+
+    @staticmethod
+    def layout_option_row(frame, label, value):
+        frame.pack(side=tk.TOP, anchor=tk.NW, padx=10, pady=5, expand=tk.YES, fill=tk.X)
+        label.pack(side=tk.LEFT)
+        value.pack(side=tk.LEFT, expand=tk.YES, fill=tk.X)
 
 
 class ImageTitlerMenuBar(tk.Menu):
@@ -105,6 +113,7 @@ class ImageTitlerMenuBar(tk.Menu):
         super().__init__(parent)
         self.parent = parent
         self.image_path = None
+        self.logo_path = None
         self.current_edit = None
         self.file_menu = None
         self.init_menu()
@@ -115,6 +124,7 @@ class ImageTitlerMenuBar(tk.Menu):
 
         self.file_menu = tk.Menu(menu, tearoff=0, postcommand=self.save_as_enabled)
         self.file_menu.add_command(label="New Image", command=self.new_image)
+        self.file_menu.add_command(label="New Logo", command=self.new_logo)
         self.file_menu.add_command(label="Save As", command=self.save_as)
 
         menu.add_cascade(label="File", menu=self.file_menu)
@@ -123,15 +133,19 @@ class ImageTitlerMenuBar(tk.Menu):
         self.image_path = tk.filedialog.askopenfilename()
         self.parent.update_view()
 
+    def new_logo(self):
+        self.logo_path = tk.filedialog.askopenfilename()
+        self.parent.update_view()
+
     def save_as(self):
         output_path = tk.filedialog.askdirectory()
         save_copy(self.image_path, self.current_edit, output_path=output_path)
 
     def save_as_enabled(self):
         if self.current_edit:
-            self.file_menu.entryconfig(1, state=tk.NORMAL)
+            self.file_menu.entryconfig(2, state=tk.NORMAL)
         else:
-            self.file_menu.entryconfig(1, state=tk.DISABLED)
+            self.file_menu.entryconfig(2, state=tk.DISABLED)
 
 
 def main():
