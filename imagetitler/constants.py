ff52099511d227946e73a6ec5c0f2ee80340b9cb
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-05-11 14:26:06 -0400

    Fixed default font issue

--- imagetitler/constants.py
+++ imagetitler/constants.py
@@ -13,7 +13,7 @@ FILE_TYPES = [('image files', ('.png', '.jpg', '.jpeg'))]
 SEPARATOR = "-"
 
 DEFAULT_BATCH_MODE = False
-DEFAULT_FONT = os.path.join(os.path.dirname(__file__), "../assets/fonts/BERNHC.TTF")
+DEFAULT_FONT = os.path.join(os.path.dirname(__file__), "assets/fonts/BERNHC.TTF")
 
 GOLD = (255, 215, 0)
 SILVER = (211, 211, 211)
